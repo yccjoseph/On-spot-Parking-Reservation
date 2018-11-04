db45d2cab6bf0d326c4bdf22d1424483ae2bf7fd
yccjoseph <joseph3yang@gmail.com>
2018-11-03 22:19:21 -0700

    add server.js

--- server/src/App.js
+++ server/src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import TimePicker from 'react-time-picker';
+import moment from 'moment';
+
+
 class App extends Component {
+  state = {
+    time: moment().format('hh:mm'),
+  }
+ 
+  onChange = time => this.setState({ time })
+
   render() {
     return (
       <div className="App">
-      Received<input type="text" name="FirstName"></input>
-      </div>
-      /*
-      <div className="App">
-        <header className="App-header">
-          
-          <p>
-            Hi Sissi. My name is Joseph. WSSA Project.
-          </p>
-        </header>
-      </div>
-      */
+      
+        Received<input type="text" name="FirstName"></input>
+
+        <TimePicker
+            onChange={this.onChange}
+            value={this.state.time}
+        />
+
+        </div>
+      
+
     );
   }
 }

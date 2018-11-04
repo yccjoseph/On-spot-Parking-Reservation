78de9104417f3d045b810c8ab87dd56212f0863f
yccjoseph <joseph3yang@gmail.com>
2018-11-03 20:29:34 -0700

    create react

---
+++ server/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      Received<input type="text" name="FirstName"></input>
+      </div>
+      /*
+      <div className="App">
+        <header className="App-header">
+          
+          <p>
+            Hi Sissi. My name is Joseph. WSSA Project.
+          </p>
+        </header>
+      </div>
+      */
+    );
+  }
+}
+
+export default App;

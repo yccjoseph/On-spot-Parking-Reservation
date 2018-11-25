f146a9b7ed1472671633171d738ed02ff00d07d2
yccjoseph <joseph3yang@gmail.com>
2018-11-25 04:02:23 -0800

    update reserve/park/leave button actions

--- server/src/App.js
+++ server/src/App.js
@@ -16,7 +16,10 @@ class App extends Component {
       reserved: 'N/A',
       time: moment().format('HH:mm')
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.check_handleClick = this.check_handleClick.bind(this)
+    this.reserve_handleClick = this.reserve_handleClick.bind(this)
+    this.park_handleClick = this.park_handleClick.bind(this)
+    this.leave_handleClick = this.leave_handleClick.bind(this)
   }
 
   getStatus = async e => {
@@ -26,16 +29,10 @@ class App extends Component {
     const body = await res.text();
     switch (body) {
       case 'a':
-        this.setState({ occupied: 'Yes', reserved: 'Yes'});
+        this.setState({ occupied: 'Yes'});
         break;
       case 'b':
-        this.setState({ occupied: 'Yes', reserved: 'No'});
-      break;
-      case 'c':
-        this.setState({ occupied: 'No', reserved: 'Yes'});
-        break;
-      case 'd':
-        this.setState({ occupied: 'No', reserved: 'No'});
+        this.setState({ occupied: 'No'});
       break;
       default:
         this.setState({ occupied: 'N/A', reserved: 'N/A'})
@@ -45,14 +42,35 @@ class App extends Component {
     await this.getStatus();
   }
   
-  handleClick = async e => {
+  // Check Availability: checks status from device
+  check_handleClick = async e => {
     await this.getStatus();
-    if (this.state.occupied === 'Yes' && this.state.reserved === 'No') {
+    if (this.state.occupied === 'Yes' || this.state.reserved === 'Yes') {
+      alert('Sorry! Not Available!');
+    }
+    else if (this.state.occupied === 'No' && this.state.reserved === 'No') {
+      alert('Hurray! Please proceed to reserve...')
+    }
+  }
+
+  // Reserve: checks status on the server (database)
+  // This should be added in the dialog box of "Check Availability" button action
+  reserve_handleClick = () => {
+    if (this.state.occupied === 'Yes' || this.state.reserved === 'Yes') {
       alert('Sorry, this time slot is unavailable for reservation. Please choose another time.');
     }
     else if (this.state.occupied === 'No' && this.state.reserved === 'No') {
       alert('Hurray! This spot is reserved for you.')
+      this.setState({ occupied: 'Yes', reserved: 'No'});
     }
+  }
+
+  park_handleClick = () => {
+    this.setState({ occupied: 'Yes', reserved: 'No'});
+  }
+
+  leave_handleClick = () => {
+    this.setState({ occupied: 'No', reserved: 'No'});
   }
 
   onChange = time => {
@@ -104,8 +122,21 @@ class App extends Component {
 
         <br/><br/>
 
-        <button onClick={this.handleClick}>
+        <button onClick={this.check_handleClick}>
           Check Availability
+        </button>
+
+        <button onClick={this.reserve_handleClick}>
+          Reserve
+        </button>
+
+        <br/><br/>
+        <button onClick={this.park_handleClick}>
+          Park
+        </button>
+
+        <button onClick={this.leave_handleClick}>
+          Leave
         </button>
       </div>
       

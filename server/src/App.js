042e1ce03bf459880d05efebe6b843b38477d8cb
yccjoseph <joseph3yang@gmail.com>
2018-12-07 15:45:28 -0800

    improve UI design

--- server/src/App.js
+++ server/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import './App.css';
 // import socketIOClient from 'socket.io-client'
-
-import TimePicker from 'react-time-picker';
+// import TimePicker from 'react-time-picker';
+import TimePicker from 'rc-time-picker';
 import moment from 'moment';
 
+const format = 'h:mm a';
 
+var popupS = require('popups');
 
 class App extends Component {
   constructor () {
@@ -44,12 +46,24 @@ class App extends Component {
   
   // Check Availability: checks status from device
   check_handleClick = async e => {
-    await this.getStatus();
+    // await this.getStatus();
     if (this.state.occupied === 'Yes' || this.state.reserved === 'Yes') {
-      alert('Sorry! Not Available!');
+      popupS.alert({
+        content: 'Sorry! Spot not available'
+      });
     }
     else if (this.state.occupied === 'No' && this.state.reserved === 'No') {
-      alert('Hurray! Please proceed to reserve...')
+      popupS.confirm({
+        content: 'Hurray, there is a spot for you!',
+        labelOk: 'Reserve',
+        onSubmit: () => {
+                popupS.alert({
+                        content: 'Great! Your spot is reserved.\nPlease be on time!'
+                });
+                this.setState({ occupied: 'No', reserved: 'Yes'});
+                document.getElementById("park").className = "btn-hover color-2";
+        }
+    });
     }
   }
 
@@ -67,10 +81,14 @@ class App extends Component {
 
   park_handleClick = () => {
     this.setState({ occupied: 'Yes', reserved: this.state.reserved});
+    document.getElementById("park").className = "btn-hover";
+    document.getElementById("leave").className = "btn-hover color-2";
   }
 
   leave_handleClick = () => {
     this.setState({ occupied: 'No', reserved: 'No'});
+    document.getElementById("park").className = "btn-hover";
+    document.getElementById("leave").className = "btn-hover";
   }
 
   onChange = time => {
@@ -78,66 +96,75 @@ class App extends Component {
     this.getStatus();
   }
   start = {
-    time: moment().format('HH:mm')
+    time: moment()
   }
 
   end = {
-    time: moment().add(2, 'hours').format('HH:mm')
+    time: moment().add(2, 'hours')
   }
 
   render() {
     return (
       <div className="App">
-        <p>Parking Space</p>
-        <select>
-          <option selected value="space0">Space #0</option>
-          <option value="space1">Space #1</option>
-          <option value="space2">Space #2</option>
-          <option value="space3">Space #3</option>
-          <option value="space4">Space #4</option>
-        </select>
+        <div class="wrapper">
+	        <h1 class="mega montserrat bold">
+		      On<span class="color-emphasis-1">Spot</span><br/>
+		      </h1>
+	      </div>
+        <span class="custom-dropdown">
+          <select>
+            <option selected value="#">Pick a spot</option>
+            <option value="space0">Space #0</option>
+            <option value="space1">Space #1</option>
+            <option value="space2">Space #2</option>
+            <option value="space3">Space #3</option>
+            <option value="space4">Space #4</option>
+          </select>
+        </span>
+        <br/><br/>
+
+        <TimePicker
+          showSecond={false}
+          defaultValue={this.start.time}
+          className="startTime"
+          onChange={this.onChange}
+          format={format}
+          use12Hours
+          inputReadOnly
+        />
+
+        <span class="prompt">to </span>
+        <TimePicker
+          showSecond={false}
+          defaultValue={this.end.time}
+          className="endTime"
+          onChange={this.onChange}
+          format={format}
+          use12Hours
+          inputReadOnly
+        />
+
+        <br/><br/>
+
+        <button class='btn-hover color-9' onClick={this.check_handleClick}>
+          Check Availability
+        </button>
+        <br/>
+
+        <button id='park' class='btn-hover' onClick={this.park_handleClick}>
+          Park
+        </button>
+
+        <button id='leave' class='btn-hover' onClick={this.leave_handleClick}>
+          Leave
+        </button>
+
+        <br/><br/><br/><br/>
         <br/><br/>
         Occupied: <span onChange={this.onClick}>{this.state.occupied}</span>
         <br/><br/>
         Reserved: <span onChange={this.onClick}>{this.state.reserved}</span>
-        <br/><br/><br/><br/>
-
-        <p>Reserve Now!</p>
-
-        <TimePicker name={"startTime"}
-            onChange={this.onChange}
-            value={this.start.time}
-            clearIcon={null}
-            disableClock={true}
-            locale={"en-US"}
-        />
-        <br/><br/>
-        <TimePicker name={"endTime"}
-            onChange={this.onChange}
-            value={this.end.time}
-            clearIcon={null}
-            disableClock={true}
-            locale={"en-US"}
-        />
-
-        <br/><br/>
-
-        <button onClick={this.check_handleClick}>
-          Check Availability
-        </button>
-
-        <button onClick={this.reserve_handleClick}>
-          Reserve
-        </button>
-
-        <br/><br/>
-        <button onClick={this.park_handleClick}>
-          Park
-        </button>
-
-        <button onClick={this.leave_handleClick}>
-          Leave
-        </button>
+        
       </div>
       
 
